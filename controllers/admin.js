04fd873ef5732abf32c36e517bf69ab20f46cefe
meozalat2022 <meozalat2002@hotmail.com>
2024-01-14 13:09:01 +0200

    delete product using mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -90,7 +90,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  // Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
