b6a47788ec75ca8d430e19d602575f1de50c38a8
meozalat2022 <meozalat2002@hotmail.com>
2024-01-24 23:19:08 +0200

    keep old user input data

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,6 +42,11 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
+    oldInput: {
+      email: email,
+      password: password,
+      confirmPassword: req.body.confirmPassword,
+    },
   });
 };
 
@@ -77,10 +82,15 @@ exports.postSignup = (req, res, next) => {
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.render("auth/signup", {
+    return res.status(422).render("auth/signup", {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: req.body.confirmPassword,
+      },
     });
   }
 
