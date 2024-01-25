356d98c3df07052044899709478f2b360aa50202
meozalat2022 <meozalat2002@hotmail.com>
2024-01-25 15:25:45 +0200

    using multer to upload images

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
-// const multer = require("multer");
+const multer = require("multer");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -39,7 +39,7 @@ const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(multer({ storage: fileStorage }).single("image"));
+app.use(multer({ dest: "images" }).single("image"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
