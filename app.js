23105a2b923bb5aafcc20cc9932b6c457bbce992
meozalat2022 <meozalat2002@hotmail.com>
2024-01-26 23:24:49 +0200

    refactoring upload/download file code

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const fileStorage = multer.diskStorage({
     cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, Math.random().toString() + "-" + file.originalname);
+    cb(null, new Date().toISOString() + "-" + file.originalname);
   },
 });
 
@@ -54,7 +54,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(
